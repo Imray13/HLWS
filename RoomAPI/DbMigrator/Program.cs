a695e39424b9b9ccc08dafa0454095729b0e3208
Bagdan Gilevich <imray13@yandex.ru>
2018-12-29 15:21:40 +0300

    Add DbMigrator

---
+++ RoomAPI/DbMigrator/Program.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using RoomAPI;
+
+namespace DbMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient();
+
+            var db = client.GetDatabase(Constants.DbName);
+
+            db.DropCollection(Constants.TableCollectionName);
+            db.CreateCollection(Constants.TableCollectionName);
+
+            db.DropCollection(Constants.PlayerCollectionName);
+            db.CreateCollection(Constants.PlayerCollectionName);
+        }
+    }
+}
